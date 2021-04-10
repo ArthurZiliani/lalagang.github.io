8552d5fa18219395f307e32681c3944ed99aa11b
Rhels Ziliani <arthur.andrade01@fatec.sp.gov.br>
2021-04-10 17:48:35 -0300

    mudanca layout

--- src/Components/footer/styles/footer.js
+++ src/Components/footer/styles/footer.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   padding: 80px 60px;
-  background: #305040;
+  background: #2e5f95;
 
   @media (max-width: 1000px) {
     padding: 70px 30px;
